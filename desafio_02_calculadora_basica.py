9301ced22f468dadfbf209310d031ea9bff7e25d
aline-cravo <aline.cravo@nova-8.com>
2024-11-12 14:55:52 -0300

    desafio 1

--- desafio_02_calculadora_basica.py
+++ desafio_02_calculadora_basica.py
@@ -1,5 +1,14 @@
 # Um menino de 10 anos de idade tinha uma calculadora de brinquedo, onde realizava operações matematicas básicas
 # como a soma e a subtração, porém ela acabou se quebrando e agora o menino não consegue mais realizar seus calculos.
-
-
 # Crie um sistema em que realize operações matematicas de soma e subtração para que o menino consiga realizar seus calculos.
+
+n1 = int(input("Adicione um nº qualquer: "))
+n2 = int(input("Adicione outro nº: "))
+operacao = input("Você quer somar os números? Digite 1 para SIm e 2 para Não: ")
+if operacao == "1":
+    resultado = n1 + n2
+elif operacao == "2":
+    resultado = n1 - n2
+else:
+    print("Você escolheu uma opção que não foi identificada. Tente novamente!")
+print(resultado)
--- desafio_03_media_aluno.py
+++ desafio_03_media_aluno.py
@@ -1,4 +1,14 @@
 # Um professor precisa anotar a média de seus alunos em sua escola, porém são diversos alunos
 # e o professor não tem tanto tempo para anotar tantas médias e dar uma nota final para cada aluno
-
 # Crie um sistema em que pergunte para o professor qual foi a nota do aluno em cada um dos 4 bimestres e calcule a nota final.
+
+b1 = float(input("Adicione a nota do 1º Bimestre: "))
+b2 = float(input("Adicione a nota do 2º Bimestre: "))
+b3 = float(input("Adicione a nota do 3º Bimestre: "))
+b4 = float(input("Adicione a nota do 4º Bimestre: "))
+media = (b1 + b2 + b3 + b4) / 4  
+print(f"A média do aluno é: {media:.2f}") #:.2f está limitando em 2 casas decimais
+if media >= 7:
+    print("Aluno aprovado")
+else:
+    print("Aluno reprovado")
